03398d0697e9acf1bca2e2405acfa031c1a4f5fb
Chad Lubrecht <chad@calubrecht.us>
2023-10-13 08:48:30 -0400

    Template Spring project

---
+++ src/main/java/us/calubrecht/lazerwiki/LazerWikiApplication.java
@@ -0,0 +1,13 @@
+package us.calubrecht.lazerwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazerWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazerWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/us/calubrecht/lazerwiki/LazerWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package us.calubrecht.lazerwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazerWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
