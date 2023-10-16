ceb5b065ea26ce991b81a12fb76476ed854bb50f
Chad Lubrecht <chad@calubrecht.us>
2023-10-16 16:30:15 -0400

    Enable run as servlet

--- src/main/java/us/calubrecht/lazerwiki/LazerWikiApplication.java
+++ src/main/java/us/calubrecht/lazerwiki/LazerWikiApplication.java
@@ -1,13 +1,26 @@
 package us.calubrecht.lazerwiki;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 
 @SpringBootApplication
-public class LazerWikiApplication {
+public class LazerWikiApplication  extends SpringBootServletInitializer {
+	Logger logger = LoggerFactory.getLogger(getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(LazerWikiApplication.class, args);
 	}
 
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		logger.info("Started Lazerwiki Application  v:{}",  "version");// VersionController.getVersion());
+	}
 }
