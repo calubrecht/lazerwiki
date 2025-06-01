5236a9f22c48169b9802ec53649f98443583bd6e
Chad Lubrecht <chad@calubrecht.us>
2025-06-01 14:29:26 -0400

    Issue #83 - Update kotlin-stdlib version to exclude from war

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "us.calubrecht"
-version = "1.0.4.5"
+version = "1.0.4.6"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -42,10 +42,10 @@ dependencies {
 	providedRuntime("mysql:mysql-connector-java:8.0.33")
 	implementation("com.github.usefulness:webp-imageio:0.5.1")
 	// Gradle will pull in an older version of this if not explicit.
-	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.10")
-	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.10")
-	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.10")
-	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.24")
+	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.24")
+	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib-common:1.9.24")
+	providedRuntime("org.jetbrains.kotlin:kotlin-stdlib:1.9.24")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
