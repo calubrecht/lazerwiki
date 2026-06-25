5153922ff863841b3a5818fafd43e1d3da0306e9
Chad Lubrecht <chad@calubrecht.us>
2026-06-25 11:07:56 -0400

    Test Coverage for edge-case in IdRepository.

--- src/test/java/us/calubrecht/lazerwiki/repository/IdRepositoryTest.java
+++ src/test/java/us/calubrecht/lazerwiki/repository/IdRepositoryTest.java
@@ -1,22 +1,40 @@
 package us.calubrecht.lazerwiki.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import us.calubrecht.lazerwiki.LazerWikiApplication;
 
 @SpringBootTest(classes = {LazerWikiApplication.class})
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class IdRepositoryTest {
 
   @Autowired IdRepository idRepository;
 
   @Test
+  @Order(1)
   void test_getNewId() {
     assertEquals(8L, idRepository.getNewId());
     assertEquals(9L, idRepository.getNewId());
+  }
+
+  @Test
+  @Order(2)  // Run last, it ruins the IdResository object
+  void test_getNewId_failure() {
+    JdbcTemplate mockTemplate = Mockito.mock(JdbcTemplate.class);
+
+    idRepository.jdbcTemplate = mockTemplate;
+    // Assert that if update does not provide an Id, throws IllegalStateException rather than NPE
+    assertThrows(IllegalStateException.class, () -> idRepository.getNewId());
   }
 }
